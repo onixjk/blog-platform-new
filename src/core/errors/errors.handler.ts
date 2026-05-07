a39a0fa438b096c9f302d57ad5142d5b1d4fbd85
Alexander <onixjkk@gmail.com>
2026-05-07 15:38:44 +0300

    fix

--- src/core/errors/errors.handler.ts
+++ src/core/errors/errors.handler.ts
@@ -5,7 +5,6 @@ import { DomainError } from './domain.error';
 import { createErrorMessages } from "../middlewares/validation/input-validtion-result.middleware";
 
 export function errorsHandler(error: unknown, res: Response): void {
-    console.error(error)
     if (error instanceof RepositoryNotFoundError) {
         const httpStatus = HttpStatus.NotFound_404;
 
