b25306f231a10644ee4f5d22a6a8d897e061d587
Alexander <onixjkk@gmail.com>
2026-05-07 12:22:35 +0300

    fix

--- src/core/middlewares/validation/query-pagination-sorting.validation-middleware.ts
+++ src/core/middlewares/validation/query-pagination-sorting.validation-middleware.ts
@@ -33,7 +33,7 @@ export function paginationAndSortingValidation<T extends string>(
             .toInt(),
 
         query('sortBy')
-            .default(Object.values(sortFieldsEnum)[0]) // Первое значение enum как дефолтное
+            .default(Object.values(sortFieldsEnum)[0])
             .isIn(allowedSortFields)
             .withMessage(
                 `Invalid sort field. Allowed values: ${allowedSortFields.join(', ')}`,
