afbf4d583154dfb4d18df8d6e36b20f64712d09f
Alexander <onixjkk@gmail.com>
2026-05-13 22:01:51 +0300

    fix

--- src/modules/testing/routers/testing.router.ts
+++ src/modules/testing/routers/testing.router.ts
@@ -1,6 +1,6 @@
 import {Router, Request, Response} from "express";
 import {HttpStatus} from "../../../core/types/http-statuses";
-import {blogCollection, postCollection} from "../../../db/mongo.db";
+import {blogCollection, postCollection, userCollection} from "../../../db/mongo.db";
 
 export const testingRouter = Router();
 
@@ -8,6 +8,7 @@ testingRouter.delete('/all-data', async (req: Request, res: Response) => {
     try {
         await postCollection.deleteMany({});
         await blogCollection.deleteMany({});
+        await userCollection.deleteMany({});
 
         res.sendStatus(HttpStatus.NoContent_204)
     } catch (e: unknown) {
