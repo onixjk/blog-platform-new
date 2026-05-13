e0d6204dc0c94ddde0a284c6d97d1fa4636405c3
Alexander <onixjkk@gmail.com>
2026-05-13 20:45:22 +0300

    fix

--- src/modules/blog/routers/handlers/get-blog-list.handler.ts
+++ src/modules/blog/routers/handlers/get-blog-list.handler.ts
@@ -2,9 +2,9 @@ import {Request, Response} from 'express';
 import {BlogQueryInput} from "../input/blog-query.input";
 import {matchedData} from "express-validator";
 import {setDefaultSortAndPaginationIfNotExist} from "../../../../core/helpers/set-default-sort-and-pagination";
-import {blogsService} from "../../application/blogs.service";
 import {errorsHandler} from "../../../../core/errors/errors.handler";
 import {HttpStatus} from "../../../../core/types/http-statuses";
+import {blogsQueryRepository} from "../../repositories/blogs.query.repository";
 
 export async function getBlogListHandler(
     req: Request,
@@ -18,7 +18,7 @@ export async function getBlogListHandler(
 
         const queryInput = setDefaultSortAndPaginationIfNotExist(sanitizedQuery);
 
-        const blogsListOutput = await blogsService.findMany(queryInput);
+        const blogsListOutput = await blogsQueryRepository.findMany(queryInput);
 
         res.status(HttpStatus.Ok_200).send(blogsListOutput);
     } catch (e: unknown) {
