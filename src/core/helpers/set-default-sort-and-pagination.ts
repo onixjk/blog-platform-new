224514693e8a507ed0ec9720e564d71bf77a284b
Alexander <onixjkk@gmail.com>
2026-05-13 22:55:52 +0300

    fix

--- src/core/helpers/set-default-sort-and-pagination.ts
+++ src/core/helpers/set-default-sort-and-pagination.ts
@@ -4,9 +4,24 @@ import { PaginationAndSorting } from '../types/pagination-and-sorting';
 export function setDefaultSortAndPaginationIfNotExist<P = string>(
     query: Partial<PaginationAndSorting<P>>,
 ): PaginationAndSorting<P> {
+    // return {
+    //     ...paginationAndSortingDefault,
+    //     ...query,
+    //     sortBy: (query.sortBy ?? paginationAndSortingDefault.sortBy) as P,
+    // };
+
+    // Принудительно приводим к числам, если значения пришли строками из query, либо берем дефолты
+    const pageNumber = Number(query.pageNumber) || paginationAndSortingDefault.pageNumber;
+    const pageSize = Number(query.pageSize) || paginationAndSortingDefault.pageSize;
+
+    // Используем || вместо ?? чтобы отсечь пустые строки ""
+    const sortBy = (query.sortBy || paginationAndSortingDefault.sortBy) as P;
+    const sortDirection = query.sortDirection || paginationAndSortingDefault.sortDirection;
+
     return {
-        ...paginationAndSortingDefault,
-        ...query,
-        sortBy: (query.sortBy || paginationAndSortingDefault.sortBy) as P,
+        pageNumber,
+        pageSize,
+        sortBy,
+        sortDirection,
     };
 }
