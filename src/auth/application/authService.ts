7d8388aa1612bfadfdb55bf4bded97d8cfd4430f
Alexander <onixjkk@gmail.com>
2026-05-19 17:49:39 +0300

    fix

--- src/auth/application/authService.ts
+++ src/auth/application/authService.ts
@@ -1,27 +1,65 @@
 import {bcryptService} from "../adapters/bcrypt.service";
 import {usersService} from "../../modules/user/application/usersService";
+import {ResultStatus} from "../../core/result/resultCode";
+import {IUserDB} from "../../modules/user/types/user.db.interface";
+import {WithId} from "mongodb";
+import {Result} from "../../core/result/result.type";
+import {jwtService} from "../adapters/jwt.service";
+
 
 export const authService = {
     async loginUser(
         loginOrEmail: string,
         password: string,
-    ): Promise<{ accessToken: string } | null> {
-        const isCorrectCredentials = await this.checkUserCredentials(loginOrEmail, password);
+    ): Promise<Result<{ accessToken: string } | null>> {
+        const result = await this.checkUserCredentials(loginOrEmail, password);
 
-        if (!isCorrectCredentials) {
-            return null;
+        if (result.status !== ResultStatus.Success) {
+            return {
+                status: ResultStatus.Unauthorized,
+                data: null,
+                errorMessage: "Unauthorized",
+                extensions: [{field: loginOrEmail, message: "Wrong credentials"}]
+            };
         }
 
-        return { accessToken: "token" };
+        const accessToken = await jwtService.createToken(result.data!._id.toString());
+
+        return {
+            status: ResultStatus.Success,
+            data: { accessToken },
+            extensions: [],
+        };
     },
 
     async checkUserCredentials(
         loginOrEmail: string,
         password: string,
-    ): Promise<boolean> {
+    ): Promise<Result<WithId<IUserDB> | null>> {
         const user = await usersService.findByLoginOrEmail(loginOrEmail);
-        if (!user) return false;
 
-        return bcryptService.checkPassword(password, user.passwordHash);
+        if (!user)
+            return {
+                status: ResultStatus.NotFound,
+                data: null,
+                errorMessage: "Not Found",
+                extensions: [{field: loginOrEmail, message: "Not Found"}]
+            };
+
+        const isPassCorrect = await bcryptService.checkPassword(password, user.passwordHash);
+
+        if (!isPassCorrect)
+            return {
+                status: ResultStatus.BadRequest,
+                data: null,
+                errorMessage: 'Bad Request',
+                extensions: [{field: 'password', message: 'Wrong password'}],
+            };
+
+        return {
+            status: ResultStatus.Success,
+            data: user,
+            extensions: [],
+        };
     },
 }
--- src/auth/routers/handlers/login-user.handler.ts
+++ src/auth/routers/handlers/login-user.handler.ts
@@ -3,6 +3,8 @@ import {errorsHandler} from "../../../core/errors/errors.handler";
 import {LoginInputDto} from "../../input/login.input-dto";
 import {authService} from "../../application/authService";
 import {HttpStatuses} from "../../../core/types/http-statuses";
+import {ResultStatus} from "../../../core/result/resultCode";
+import {resultCodeToHttpException} from "../../../core/result/resultCodeToHttpException";
 
 export async function loginUserHandler(
     req: Request<{}, {}, LoginInputDto>,
@@ -10,12 +12,14 @@ export async function loginUserHandler(
 ) {
     try {
         const {loginOrEmail, password} = req.body;
-        const accessToken = await authService.loginUser(loginOrEmail, password);
 
-        if (!accessToken)
-            res.sendStatus(HttpStatuses.Unauthorized_401);
+        const result = await authService.loginUser(loginOrEmail, password);
 
-        res.status(HttpStatuses.Ok_200).send(accessToken);
+        if (result.status !== ResultStatus.Success) {
+            return res.status(resultCodeToHttpException(result.status)).send(result.extensions);
+        }
+
+        return res.status(HttpStatuses.Ok_200).send({ accessToken: result.data!.accessToken });
     } catch (e: unknown) {
         errorsHandler(e, res);
     }
