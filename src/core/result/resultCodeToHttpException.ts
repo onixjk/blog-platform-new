4a1e9f689f74d320cc527590649b9a1fb8fb8d40
Alexander <onixjkk@gmail.com>
2026-05-19 17:59:36 +0300

    fix

--- src/core/result/resultCodeToHttpException.ts
+++ src/core/result/resultCodeToHttpException.ts
@@ -1,12 +1,18 @@
-import { ResultStatus } from "./resultCode";
+import {ResultStatus} from "./resultCode";
 import {HttpStatuses} from "../types/http-statuses";
 
 export const resultCodeToHttpException = (resultCode: ResultStatus): number => {
     switch (resultCode) {
+        case ResultStatus.Success:
+            return HttpStatuses.Ok_200
         case ResultStatus.BadRequest:
             return HttpStatuses.BadRequest_400;
+        case ResultStatus.Unauthorized:
+            return HttpStatuses.Unauthorized_401
         case ResultStatus.Forbidden:
             return HttpStatuses.Forbidden_403;
+        case ResultStatus.NotFound:
+            return HttpStatuses.NotFound_404
         default:
             return HttpStatuses.InternalServerError_500;
     }
