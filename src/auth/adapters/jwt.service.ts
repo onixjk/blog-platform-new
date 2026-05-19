10269961c5579bfa20cedb084741196ce5b188c7
Alexander <onixjkk@gmail.com>
2026-05-19 14:24:27 +0300

    fix

--- src/auth/adapters/jwt.service.ts
+++ src/auth/adapters/jwt.service.ts
@@ -1,28 +1,28 @@
-// import jwt from "jsonwebtoken";
-// import {appConfig} from "../common/config";
-//
-//
-//
-// export const jwtService = {
-//     async createToken(userId: string): Promise<string> {
-//         return jwt.sign({userId}, appConfig.AC_SECRET, {
-//             expiresIn: appConfig.AC_TIME as any,
-//         });
-//     },
-//     async decodeToken(token: string): Promise<any> {
-//         try {
-//             return jwt.decode(token);
-//         } catch (e: unknown) {
-//             console.error("Can't decode token", e);
-//             return null;
-//         }
-//     },
-//     async verifyToken(token: string): Promise<{ userId: string } | null> {
-//         try {
-//             return jwt.verify(token, appConfig.AC_SECRET) as { userId: string };
-//         } catch (error) {
-//             console.error("Token verify some error");
-//             return null;
-//         }
-//     }
-// }
+import jwt from "jsonwebtoken";
+import {appConfig} from "../common/config";
+
+export const jwtService = {
+    async createToken(userId: string): Promise<string> {
+        return jwt.sign({userId}, appConfig.AC_SECRET, {
+            expiresIn: appConfig.AC_TIME as any,
+        });
+    },
+
+    async decodeToken(token: string): Promise<any> {
+        try {
+            return jwt.decode(token);
+        } catch (e: unknown) {
+            console.error("Can't decode token", e);
+            return null;
+        }
+    },
+
+    async verifyToken(token: string): Promise<{ userId: string } | null> {
+        try {
+            return jwt.verify(token, appConfig.AC_SECRET) as { userId: string };
+        } catch (error) {
+            console.error("Token verify some error");
+            return null;
+        }
+    }
+}
