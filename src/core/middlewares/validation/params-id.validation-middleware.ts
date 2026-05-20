af2c2833912a3f5406e7c87b55a7222219215b21
Alexander <onixjkk@gmail.com>
2026-05-20 17:18:12 +0300

    fix

--- src/core/middlewares/validation/params-id.validation-middleware.ts
+++ src/core/middlewares/validation/params-id.validation-middleware.ts
@@ -2,16 +2,16 @@ import { param } from 'express-validator';
 
 export const idValidation = param('id')
     .exists()
-    .withMessage('ID is required') // Проверка на наличие
+    .withMessage('ID is required')
     .isString()
-    .withMessage('ID must be a string') // Проверка, что это строка
+    .withMessage('ID must be a string')
     .isMongoId()
     .withMessage('Invalid format ObjectId');
 
 export const blogIdValidation = param('blogId')
     .exists()
-    .withMessage('ID is required') // Проверка на наличие
+    .withMessage('ID is required')
     .isString()
-    .withMessage('ID must be a string') // Проверка, что это строка
+    .withMessage('ID must be a string')
     .isMongoId()
     .withMessage('Invalid format ObjectId');
