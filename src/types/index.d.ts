4f9852a324cd00ce9b9f70aa00c36fa744683f64
Alexander <onixjkk@gmail.com>
2026-05-26 17:27:32 +0300

    fix

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,8 +3,8 @@ import 'express';
 declare global {
     namespace Express {
         export interface Request {
-            user?: {
-                id: string;
+            user: {
+                id: string | null;
             };
         }
     }
