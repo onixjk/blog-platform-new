ecea82e41fbf4edd70f3b4ab4b9783e5aa1eb17f
Alexander <onixjkk@gmail.com>
2026-05-28 22:12:14 +0300

    fix

--- src/auth/routers/handlers/post-registration.handler.ts
+++ src/auth/routers/handlers/post-registration.handler.ts
@@ -4,17 +4,22 @@ import {authService} from "../../application/authService";
 import {HttpStatuses} from "../../../core/types/http-statuses";
 import {ResultStatus} from "../../../core/result/resultCode";
 import {resultCodeToHttpException} from "../../../core/result/resultCodeToHttpException";
+import {errorsHandler} from "../../../core/errors/errors.handler";
 
 export async function registrationHandler(
     req: Request<{}, {}, UserInputDto>,
     res: Response,
 ) {
-    const {login, password, email} = req.body;
+    try {
+        const {login, password, email} = req.body;
 
-    const result = await authService.registerUser(login, password, email);
+        const result = await authService.registerUser(login, password, email);
 
-    if (result.status !== ResultStatus.NoContent)
-        return res.status(resultCodeToHttpException(result.status)).send(result.extensions);
+        if (result.status !== ResultStatus.NoContent)
+            return res.status(resultCodeToHttpException(result.status)).send(result.extensions);
 
-    return res.sendStatus(HttpStatuses.NoContent_204)
+        return res.sendStatus(HttpStatuses.NoContent_204)
+    } catch (e: unknown) {
+        errorsHandler(e, res);
+    }
 }
