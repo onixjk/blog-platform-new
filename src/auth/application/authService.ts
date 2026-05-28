ca0a0e8d493621a3bfd204fd653d256ac030a715
Alexander <onixjkk@gmail.com>
2026-05-28 22:48:20 +0300

    fix

--- src/auth/application/authService.ts
+++ src/auth/application/authService.ts
@@ -99,25 +99,15 @@ export const authService = {
         };
 
         await usersRepository.create(newUser);
-        try {
-            await nodemailerService
-                .sendEmail(
-                    newUser.email,
-                    newUser.emailConfirmation.confirmationCode,
-                    emailExamples.registrationEmail
-                )
-            // .catch(er => console.error('error in send email:', er));
-        } catch (e) {
-            console.error('Критическая ошибка при отправке email на Vercel:', e);
-        }
 
-        // nodemailerService
-        //     .sendEmail(
-        //         newUser.email,
-        //         newUser.emailConfirmation.confirmationCode,
-        //         emailExamples.registrationEmail
-        //     )
-        //     .catch(er => console.error('error in send email:', er));
+        nodemailerService
+            .sendEmail(
+                newUser.email,
+                newUser.emailConfirmation.confirmationCode,
+                emailExamples.registrationEmail
+            )
+            .catch(er => console.error('error in send email:', er));
+
         return {
             status: ResultStatus.NoContent,
             data: null,
