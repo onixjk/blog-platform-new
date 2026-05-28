2d7b126a8b73f97b7dea80e4f06f7db867c56c3c
Alexander <onixjkk@gmail.com>
2026-05-28 23:26:05 +0300

    fix

--- src/auth/adapters/email-examples.ts
+++ src/auth/adapters/email-examples.ts
@@ -2,7 +2,6 @@ export const emailExamples = {
     registrationEmail(code: string) {
         return ` <h1>Thank for your registration</h1>
                <p>To finish registration please follow the link below:<br>
-<!--                  <a href='https://somesite.com/confirm-email?code=${code}'>complete registration</a>-->
                   <a href='https://some-front.com/confirm-registration?code=${code}'>complete registration</a>
               </p>`
     },
