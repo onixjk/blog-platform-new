d83b4c16aaca12a9c00a3d98eab2db4b53269183
Alexander <onixjkk@gmail.com>
2026-05-28 22:54:15 +0300

    fix

--- src/auth/adapters/nodemailer.service.ts
+++ src/auth/adapters/nodemailer.service.ts
@@ -15,7 +15,7 @@ export const nodemailerService = {
         });
 
         let info = await transporter.sendMail({
-            from: '"Kek 👻" <codeSender>',
+            from: '"Kek 👻" <nkplwow@gmail.com>',
             to: email,
             subject: "Your code is here",
             html: template(code), // html body
