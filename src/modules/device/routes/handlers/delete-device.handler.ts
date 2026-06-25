5ea737187f713417224c4d32820302fbf279d7ea
Alexander <onixjkk@gmail.com>
2026-06-25 22:09:14 +0300

    fix

--- src/modules/device/routes/handlers/delete-device.handler.ts
+++ src/modules/device/routes/handlers/delete-device.handler.ts
@@ -5,17 +5,17 @@ import { ResultStatus } from "../../../../core/result/resultCode";
 import { resultCodeToHttpException } from "../../../../core/result/resultCodeToHttpException";
 
 export async function deleteDeviceHandler(
-    req: Request<{ deviceId: string }, {}, {}, {}>,
+    req: Request<{ id: string }, {}, {}, {}>,
     res: Response
 ) {
     const userId = req.user?.id;
-    const deviceId = req.params.deviceId;
+    const deviceId = req.params.id;
 
     if (!userId || !deviceId) {
         return res.sendStatus(HttpStatuses.Unauthorized_401);
     }
 
-    const result = await deviceService.deleteSessionById(deviceId, userId);
+    const result = await deviceService.deleteSessionById(userId, deviceId);
 
     if (result.status !== ResultStatus.NoContent) {
         return res
