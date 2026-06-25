8bb24073c425005a0fcc74be856fac583388d589
Alexander <onixjkk@gmail.com>
2026-06-25 16:31:14 +0300

    fix

--- src/modules/device/routes/device.router.ts
+++ src/modules/device/routes/device.router.ts
@@ -12,7 +12,7 @@ export const deviceRouter = Router({});
 
 deviceRouter
     .get('/devices',
-        idValidation,
+        deviceIdValidation,
         inputValidationResultMiddleware,
         getDeviceListHandler
     )
