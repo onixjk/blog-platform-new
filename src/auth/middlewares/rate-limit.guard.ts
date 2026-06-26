05bd85f373ca617fd6c4012e877d4816398ba86b
Alexander <onixjkk@gmail.com>
2026-06-26 11:21:45 +0300

    fix

--- src/auth/middlewares/rate-limit.guard.ts
+++ src/auth/middlewares/rate-limit.guard.ts
@@ -2,38 +2,38 @@ import { Request, Response, NextFunction } from 'express';
 import { apiRequestsCollection } from "../../db/mongo.db";
 import { HttpStatuses } from "../../core/types/http-statuses";
 
-export const rateLimitGuard = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const clientIp = (req.headers['x-forwarded-for'] as string) || req.ip || 'unknown';
-    const currentUrl = req.originalUrl || req.url; // Важно трекать конкретный URL
-    const currentTime = new Date();
-
-    const tenSecondsAgo = new Date(currentTime.getTime() - 10 * 1000);
-
-    // 1. Считаем в БД количество запросов от этого IP на этот URL за последние 10 секунд
-    const currentAttemptsCount = await apiRequestsCollection.countDocuments({
-        ip: clientIp,
-        url: currentUrl,
-        date: { $gte: tenSecondsAgo } // Дата должна быть больше или равна времени 10 секунд назад
-    });
-
-    // 2. Если уже набралось 5 или более запросов — блокируем (429)
-    if (currentAttemptsCount >= 5) {
-        return res.sendStatus(HttpStatuses.TooManyRequests_429);
-    }
-
-    // 3. Если лимит не превышен, логируем текущий запрос в БД
-    await apiRequestsCollection.insertOne({
-        ip: clientIp,
-        url: currentUrl,
-        date: currentTime
-    });
-
-    return next();
-};
+// export const rateLimitGuard = async (
+//     req: Request,
+//     res: Response,
+//     next: NextFunction
+// ) => {
+//     const clientIp = (req.headers['x-forwarded-for'] as string) || req.ip || 'unknown';
+//     const currentUrl = req.originalUrl || req.url; // Важно трекать конкретный URL
+//     const currentTime = new Date();
+//
+//     const tenSecondsAgo = new Date(currentTime.getTime() - 10 * 1000);
+//
+//     // 1. Считаем в БД количество запросов от этого IP на этот URL за последние 10 секунд
+//     const currentAttemptsCount = await apiRequestsCollection.countDocuments({
+//         ip: clientIp,
+//         url: currentUrl,
+//         date: { $gte: tenSecondsAgo } // Дата должна быть больше или равна времени 10 секунд назад
+//     });
+//
+//     // 2. Если уже набралось 5 или более запросов — блокируем (429)
+//     if (currentAttemptsCount >= 5) {
+//         return res.sendStatus(HttpStatuses.TooManyRequests_429);
+//     }
+//
+//     // 3. Если лимит не превышен, логируем текущий запрос в БД
+//     await apiRequestsCollection.insertOne({
+//         ip: clientIp,
+//         url: currentUrl,
+//         date: currentTime
+//     });
+//
+//     return next();
+// };
 
 
 
