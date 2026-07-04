b5a5e38a2f606265e3a4a682be2fd37d27b9823a
Alexander <onixjkk@gmail.com>
2026-07-04 16:33:01 +0300

    fix

--- src/modules/auth/adapters/email-examples.ts
+++ src/modules/auth/adapters/email-examples.ts
@@ -9,7 +9,7 @@ export class EmailExamples {
     passwordRecoveryEmail(code: string) {
         return `<h1>Password recovery</h1>
         <p>To finish password recovery please follow the link below:
-            <a href='https://some-front.com/password-recovery?code=${code}'>recovery password</a>
+            <a href='hhttps://somesite.com/password-recovery?recoveryCode=${code}'>recovery password</a>
         </p>`
     }
 }
