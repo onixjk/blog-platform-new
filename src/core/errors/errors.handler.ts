24ae0ac6ce4e67acbe97e0c4febff48a981b71fa
Alexander <onixjkk@gmail.com>
2026-07-09 18:09:50 +0300

    fix

--- src/core/errors/errors.handler.ts
+++ src/core/errors/errors.handler.ts
@@ -5,6 +5,7 @@ import {DomainError} from './domain.error';
 import {createErrorMessages} from "../middlewares/validation/input-validation-result.middleware";
 
 export function errorsHandler(error: unknown, res: Response): void {
+    console.error("=== КРИТИЧЕСКАЯ ОШИБКА НА СЕРВЕРЕ ===", error);
 
     if (error instanceof RepositoryNotFoundError) {
         const httpStatus = HttpStatuses.NotFound_404;
