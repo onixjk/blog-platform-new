f438fbc446d3af883022f0de14f1e1cc199b50e3
Alexander <onixjkk@gmail.com>
2026-07-23 22:40:30 +0300

    fix

--- src/db/mongo.db.ts
+++ src/db/mongo.db.ts
@@ -1,74 +1,3 @@
-// import mongoose from 'mongoose';
-// import { Collection, Db, MongoClient } from 'mongodb';
-// import { SETTINGS } from '../core/settings/settings';
-// import { Blog } from "../modules/blog/types/blog";
-// import { Post } from "../modules/post/types/post";
-// import { IUserDB } from "../modules/user/types/user.db.interface";
-// import { Comment } from "../modules/comment/types/comment";
-// import { Session } from "../modules/auth/types/session";
-// import { Device } from "../modules/device/types/device.";
-// import { ApiRequestLog } from "../modules/auth/types/api-request-log";
-//
-// const BLOG_COLLECTION_NAME = 'blogs';
-// const POST_COLLECTION_NAME = 'posts';
-// const USER_COLLECTION_NAME = 'users';
-// const COMMENTS_COLLECTION_NAME = 'comments';
-// const SESSIONS_COLLECTION_NAME = 'sessions';
-// const DEVICE_COLLECTION_NAME = 'devices';
-// const API_REQUEST_COLLECTION_NAME = 'apiRequests';
-//
-// export let client: MongoClient;
-//
-// export let blogCollection: Collection<Blog>;
-// export let postCollection: Collection<Post>;
-// export let userCollection: Collection<IUserDB>;
-// export let commentCollection: Collection<Comment>;
-// export let sessionCollection: Collection<Session>;
-// export let deviceCollection: Collection<Device>;
-// export let apiRequestsCollection: Collection<ApiRequestLog>;
-//
-// // Подключения к бд
-// export async function runDB(url: string): Promise<void> {
-//     // client = new MongoClient(url);
-//     // const db: Db = client.db(SETTINGS.DB_NAME);
-//
-//     //Инициализация коллекций
-//     // blogCollection = db.collection<Blog>(BLOG_COLLECTION_NAME);
-//     // postCollection = db.collection<Post>(POST_COLLECTION_NAME);
-//     // userCollection = db.collection<IUserDB>(USER_COLLECTION_NAME);
-//     // commentCollection = db.collection<Comment>(COMMENTS_COLLECTION_NAME);
-//     // sessionCollection = db.collection<Session>(SESSIONS_COLLECTION_NAME);
-//     // deviceCollection = db.collection<Device>(DEVICE_COLLECTION_NAME);
-//     // apiRequestsCollection = db.collection<ApiRequestLog>(API_REQUEST_COLLECTION_NAME);
-//
-//     try {
-//         await mongoose.connect(url);
-//         // await db.command({ ping: 1 });
-//         console.log('✅ Connected to the database');
-//
-//         // await apiRequestsCollection.createIndex({ date: 1 }, { expireAfterSeconds: 10 });
-//         // await sessionCollection.createIndex({ expireDate: 1 }, { expireAfterSeconds: 0 });
-//         // console.log('✅ TTL index for sessionCollection created/verified');
-//     } catch (e) {
-//         // await client.close();
-//         await mongoose.disconnect()
-//         throw new Error(`❌ Database not connected: ${ e }`);
-//     }
-//
-//     // try {
-//     //     await client.connect();
-//     //     await db.command({ ping: 1 });
-//     //     console.log('✅ Connected to the database');
-//     //
-//     //     await apiRequestsCollection.createIndex({ date: 1 }, { expireAfterSeconds: 10 });
-//     //     await sessionCollection.createIndex({ expireDate: 1 }, { expireAfterSeconds: 0 });
-//     //     console.log('✅ TTL index for sessionCollection created/verified');
-//     // } catch (e) {
-//     //     await client.close();
-//     //     throw new Error(`❌ Database not connected: ${ e }`);
-//     // }
-// }
-
 import mongoose, { Schema } from 'mongoose';
 import { SETTINGS } from '../core/settings/settings';
 import { Blog } from "../modules/blog/types/blog";
@@ -91,13 +20,13 @@ const BlogSchema = new Schema<Blog>({}, { strict: false });
 const PostSchema = new Schema<Post>({}, { strict: false });
 const UserSchema = new Schema<IUserDB>({}, { strict: false });
 const CommentSchema = new Schema<Comment>({}, { strict: false });
-// const LikesSchema = new Schema<Like>({}, { strict: false });
-export const LikesSchema = new Schema<Like>({
-    commentId: { type: String, required: true },
-    userId: { type: String, required: true },
-    status: { type: String, required: true },
-    createdAt: { type: String, required: true }
-});
+const LikesSchema = new Schema<Like>({}, { strict: false });
+// export const LikesSchema = new Schema<Like>({
+//     commentId: { type: String, required: true },
+//     userId: { type: String, required: true },
+//     status: { type: String, required: true },
+//     createdAt: { type: String, required: true }
+// });
 
 export let BlogModel = mongoose.model<Blog>('blogs', BlogSchema);
 export let PostModel = mongoose.model<Post>('posts', PostSchema);
