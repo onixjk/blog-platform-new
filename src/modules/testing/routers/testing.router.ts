bcb5c45f0bba7ea0110b52f8717d7d288ec786be
Alexander <onixjkk@gmail.com>
2026-07-30 22:18:53 +0300

    fix

--- src/modules/testing/routers/testing.router.ts
+++ src/modules/testing/routers/testing.router.ts
@@ -1,31 +1,11 @@
 import { Request, Response, Router } from "express";
 import { HttpStatuses } from "../../../core/types/http-statuses";
 import mongoose from "mongoose";
-import {
-    ApiRequestsModel,
-    BlogModel, CommentLikeModel,
-    CommentModel,
-    PostLikeModel,
-    PostModel,
-    SessionModel,
-    UserModel
-} from "../../../db/mongo.db";
-
 
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
     try {
-        // await Promise.all([
-        //     PostModel.deleteMany({}),
-        //     BlogModel.deleteMany({}),
-        //     UserModel.deleteMany({}),
-        //     CommentModel.deleteMany({}),
-        //     SessionModel.deleteMany({}),
-        //     ApiRequestsModel.deleteMany({}),
-        //     CommentLikeModel.deleteMany({}),
-        //     PostLikeModel.deleteMany({}),
-        // ]);
 
         await mongoose.connection.dropDatabase();
 
