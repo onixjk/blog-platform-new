90b1d44226e47a498fc2defdfd8b5f79cd5b16ca
Alexander <onixjkk@gmail.com>
2026-07-30 21:12:03 +0300

    fix

--- src/modules/testing/routers/testing.router.ts
+++ src/modules/testing/routers/testing.router.ts
@@ -1,30 +1,24 @@
 import { Request, Response, Router } from "express";
 import { HttpStatuses } from "../../../core/types/http-statuses";
-import {
-    ApiRequestsModel,
-    BlogModel,
-    CommentModel,
-    CommentLikeModel, PostLikeModel,
-    PostModel,
-    SessionModel,
-    UserModel
-} from "../../../db/mongo.db";
+import mongoose from "mongoose";
 
 
 export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
     try {
-        await Promise.all([
-            PostModel.deleteMany({}),
-            BlogModel.deleteMany({}),
-            UserModel.deleteMany({}),
-            CommentModel.deleteMany({}),
-            SessionModel.deleteMany({}),
-            ApiRequestsModel.deleteMany({}),
-            CommentLikeModel.deleteMany({}),
-            PostLikeModel.deleteMany({}),
-        ]);
+        // await Promise.all([
+        //     PostModel.deleteMany({}),
+        //     BlogModel.deleteMany({}),
+        //     UserModel.deleteMany({}),
+        //     CommentModel.deleteMany({}),
+        //     SessionModel.deleteMany({}),
+        //     ApiRequestsModel.deleteMany({}),
+        //     CommentLikeModel.deleteMany({}),
+        //     PostLikeModel.deleteMany({}),
+        // ]);
+
+        await mongoose.connection.dropDatabase();
 
         res.sendStatus(HttpStatuses.NoContent_204)
     } catch (e: unknown) {
