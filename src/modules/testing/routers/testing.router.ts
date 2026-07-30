fb0ae5602259ad47986583e6bcc1de3703f191c6
Alexander <onixjkk@gmail.com>
2026-07-30 22:07:25 +0300

    fix

--- src/modules/testing/routers/testing.router.ts
+++ src/modules/testing/routers/testing.router.ts
@@ -16,18 +16,18 @@ export const testingRouter = Router();
 
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
     try {
-        await Promise.all([
-            PostModel.deleteMany({}),
-            BlogModel.deleteMany({}),
-            UserModel.deleteMany({}),
-            CommentModel.deleteMany({}),
-            SessionModel.deleteMany({}),
-            ApiRequestsModel.deleteMany({}),
-            CommentLikeModel.deleteMany({}),
-            PostLikeModel.deleteMany({}),
-        ]);
+        // await Promise.all([
+        //     PostModel.deleteMany({}),
+        //     BlogModel.deleteMany({}),
+        //     UserModel.deleteMany({}),
+        //     CommentModel.deleteMany({}),
+        //     SessionModel.deleteMany({}),
+        //     ApiRequestsModel.deleteMany({}),
+        //     CommentLikeModel.deleteMany({}),
+        //     PostLikeModel.deleteMany({}),
+        // ]);
 
-        // await mongoose.connection.dropDatabase();
+        await mongoose.connection.dropDatabase();
 
         res.sendStatus(HttpStatuses.NoContent_204)
     } catch (e: unknown) {
